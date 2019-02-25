3830a5ef408d06e8bd72ab25e595df04dd2cd2af
kor1k <koortttt@gmail.com>
2019-02-25 21:55:05 +0200

    TODO are on board

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,7 @@
-// function passGen() {
-//     let userName = prompt('Enter ur name')
-//     console.log('user name is: ' + userName)
-//     let lengthOfPass = prompt('Enter length of pass')
-//     console.log('length of pass is: ' + lengthOfPass)
-//
-//     let gen = Math.random()
-// }
-// passGen();
-
-
 function generatePassword() {
-    let length = 10,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
+    let length = 10;
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let retVal = '';
     for (let i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
@@ -24,5 +13,9 @@ let generate = document.getElementById('elem');
 generate.onclick = function () {
     alert(generatePassword());
 };
+
+//TODO: Добавить диапазон длинны ОТ и До
+//TODO: Кол-во паролей, которые должны быть выведены
+//TODO: Кол-во выводимых паролей
 
 
