34e58824923b70b83070c12aa8c89b021c63c7d6
kor1k <koortttt@gmail.com>
2019-02-26 13:02:35 +0200

    added check for 0

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+function reloadBtn() {
+    location.reload();
+}
+
 //-------------------------
 // Only numbers password
 // let charset = '1234567890';
@@ -11,15 +15,25 @@
 //Charset with symbols, letters and numbers
 // let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}:"?|<>~'
 
+
 function generatePassword() {
     // length of password
+    let notValid = 0;
     let lenOfPass = +prompt('Enter your length of password');
+    if (isNaN(lenOfPass) === true) {
+        alert('There is no numbers! Please try one more time ')
+    }
+    if (lenOfPass === false) {
+        alert('You pressed Cancel')
+    }
+    if (lenOfPass === notValid) {
+        alert('0 does not exist!')
+    }
     let length = lenOfPass;
     // charset
     let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     // value of password
     let retVal = '';
-    1
     // cycle for genering password
     for (let i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
