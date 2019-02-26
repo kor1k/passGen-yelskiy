51b693fd0c6cc8f9bb452d269c2e2f8886c47dda
kor1k <koortttt@gmail.com>
2019-02-26 12:37:49 +0200

    added comments

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,33 @@
+//-------------------------
+// Only numbers password
+// let charset = '1234567890';
+//---------------------------
+// Only small letters
+//let charset = 'qwertyuiopasdfghjklzxcvbnm'
+//---------------------------
+//Only big letters
+// let charset = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+//-------------------------
+//Charset with symbols, letters and numbers
+// let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}:"?|<>~'
+
 function generatePassword() {
+    // length of password
     let length = 10;
+    // charset
     let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    // value of password
     let retVal = '';
+    // cycle for genering password
     for (let i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
+    // login in console
     console.log(retVal);
     return retVal;
 }
 
+// taking element from HTML by Id and giving function to this element
 let generate = document.getElementById('elem');
 generate.onclick = function () {
     alert(generatePassword());
@@ -17,5 +36,3 @@ generate.onclick = function () {
 //TODO: Добавить диапазон длинны ОТ и До
 //TODO: Кол-во паролей, которые должны быть выведены
 //TODO: Кол-во выводимых паролей
-
-
