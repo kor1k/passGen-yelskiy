1995dfc8291ebf6c13f3d5fa318fd4ab16d38035
kor1k <koortttt@gmail.com>
2019-02-28 12:22:29 +0200

    added checks

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,49 @@
+//genering random number
 function rand(lower, upper) {
     return Math.floor(Math.random() * (upper - lower) + lower);
 }
 
 document.getElementById('elem').onclick = function randAction() {
+    let limitVal = 2147483647;
     // taking value from low input
     let lower = +document.getElementById('lower').value;
     // taking value from upper input
     let upper = +document.getElementById('upper').value;
     // taking value from quantity input
     let quantity = +document.getElementById('quantity').value;
+    // check for INT limits
+    if (lower > limitVal) {
+        alert('Number is not valid! Check text in head of page');
+        return false;
+    }
+
+    if (upper > limitVal) {
+        alert('Number is not valid! Check text in head of page');
+        return false;
+    }
+    if (quantity > limitVal) {
+        alert('Number is not valid! Check text in head of page');
+        return false;
+
+    }
     // check for l > u
     if (lower > upper) {
-        alert('wrong, lower is bigger than upper or there is number with -   ');
+        alert('Wrong, lower is bigger than upper or there is number with -   ');
         return false;
     }
     // check for isNaN quantity
-    if (isNaN(quantity) == true) {
-        alert('u have wrong quantity. check it')
+    if (isNaN(lower) == true) {
+        alert('Wrong! Input is not a number!');
+        return false;
     }
     // check for isNaN lower and upper
     if (isNaN(lower || upper) == true) {
-        alert('there is not a number!')
+        alert('Wrong! Input is not a number!');
+        return false;
+    }
+    if (isNaN(quantity) == true) {
+        alert('Wrong! Input is not a number!');
+        return false;
     }
     //cycle for type n-th quantity of numbers
     for (let i = 0; i < quantity; i++) {
@@ -42,3 +65,5 @@ function consoleClear() {
 //TODO: Добавить проверку на 0 || Done
 //TODO: Добавить проверку на букву || Done
 //TODO: Добавить проверку на символ || Done
+
+//TODO: ограничения на инпуты, проверка ловера на стрингу
