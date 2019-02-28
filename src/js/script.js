83bd55c7e136c189e898455e8cc663e087e4cd1c
kor1k <koortttt@gmail.com>
2019-02-28 10:29:12 +0200

    added checkers for Number, - and symbols and letters

--- src/js/script.js
+++ src/js/script.js
@@ -6,16 +6,26 @@ document.getElementById('elem').onclick = function () {
     let lower = +document.getElementById('lower').value;
     let upper = +document.getElementById('upper').value;
     let quantity = +document.getElementById('quantity').value;
+    if (lower > upper) {
+        alert('wrong, lower is bigger than upper or there is number with -   ');
+        return false;
+    }
+    if (isNaN(quantity) == true){
+        alert('u have wrong quantity. check it')
+    }
+    if (isNaN(lower || upper) == true) {
+        alert('there is not a number!')
+    }
     for (let i = 0; i < quantity; i++) {
         console.log(rand(lower, upper))
     }
 };
 
-//TODO: Кол-во выводимых паролей ||
-//TODO: Добавить диапазон длинны ОТ и До ||
-//TODO: Добавить проверку на -число ||
-//TODO: Кол-во сиволов, который должны быть выведены ||
-//TODO: Вывод сгенерированого числа в HTML || Done
-//TODO: Добавить проверку на 0 || Done
-//TODO: Добавить проверку на букву
+
+//TODO: Кол-во выводимых паролей || Done
+//TODO: Добавить диапазон длинны ОТ и До || Done
+//TODO: Добавить проверку на -число || Done
+//TODO: Вывод сгенерированого числа в HTML ||
+//TODO: Добавить проверку на 0 ||
+//TODO: Добавить проверку на букву || Done
 //TODO: Добавить проверку на символ
